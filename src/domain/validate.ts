b87daafaf30b920a727fce77fdf387451b8b5455
jungjinbeom <insunok0715@naver.com>
2024-04-08 01:13:21 +0900

    유효성 검사 함수 명 수정

--- src/domain/validate.ts
+++ src/domain/validate.ts
@@ -1,12 +1,12 @@
 import { MAX_MONTH, MIN_MONTH } from './constant';
 
-export const validNumber = (value: string) => /^(?:[1-9][0-9]{0,2}|)$/.test(value);
+export const isValidNumber = (value: string) => /^(?:[1-9][0-9]{0,2}|)$/.test(value);
 
-export const validCardPassword = (value: string) => /^(?:[1-9][0-9]{0,0}|)$/.test(value);
+export const isValidCardPassword = (value: string) => /^(?:[1-9][0-9]{0,0}|)$/.test(value);
 
-export const validCardNumber = (value: string) => /^(?:[1-9][0-9]{0,3}|)$/.test(value);
+export const isValidCardNumber = (value: string) => /^(?:[1-9][0-9]{0,3}|)$/.test(value);
 
-export const validExpirationDate = (value: string) =>
+export const isValidExpirationDate = (value: string) =>
   MIN_MONTH <= Number(value) && MAX_MONTH >= Number(value);
 
 export const isFailed = (state = '', limit: number) => state.length === limit;
